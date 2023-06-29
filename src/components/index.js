6f63c9f3df848b89a8fc282e3753b5a14605043a
Thu Dang <>
2023-06-29 13:57:11 -0500

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,25 @@
+import NavBar from "./NavBar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Business from "./Business";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+import GetStarted from "./GetStarted";
+
+export {
+  NavBar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+  GetStarted
+};
